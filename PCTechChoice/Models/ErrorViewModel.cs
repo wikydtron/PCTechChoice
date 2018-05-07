1ac25781bebd02554ce55bc52fa836c317502763
fboss <fboss@MSI>
2018-05-07 13:12:38 -0300

    Add project files.

---
+++ PCTechChoice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PCTechChoice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
